0c3f8d4735a9f757b23853e4ec05fa959ae5e3db
daniel <daniel.liboos@gmail.com>
2019-03-14 16:47:01 -0300

    Segundo Commit

--- src/main/java/bcd/Exemplo1.java
+++ src/main/java/bcd/Exemplo1.java
@@ -1,5 +1,39 @@
 package bcd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class Exemplo1 {
+
+    private static final String dbPath = "src/main/resources/lab01.sqlite";
+
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Scanner teclado = new Scanner(System.in);
+        //ler string
+        String s = teclado.nextLine();
+        teclado.next();//consumir caractere extra do ENTER CR/LF
+        //ler int
+        int i = teclado.nextInt();
+
+
+        System.out.println("Ola mundo!");
+
+        Class.forName("org.sqlite.JDBC");
+
+        Connection conexao = DriverManager.getConnection("jdbc:sqlite:"+dbPath);
+
+        Statement stmt = conexao.createStatement();
+
+        String query = "INSERT INTO Aluno VALUES (2,'Juca',80,180,'email@do.com')";
+
+        stmt.executeUpdate(query);
+
+        stmt.close();
+        conexao.close();
+    }
 
 }
